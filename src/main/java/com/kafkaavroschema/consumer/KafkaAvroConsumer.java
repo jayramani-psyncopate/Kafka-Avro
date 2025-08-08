2531d1b597d0e408a269de78dcb9ec00588cb4ac
jayramani-psyncopate <jramani@psyncopate.com>
2025-08-08 16:12:06 -0400

    added a dead letter topics logic on consumer side

--- src/main/java/com/kafkaavroschema/consumer/KafkaAvroConsumer.java
+++ src/main/java/com/kafkaavroschema/consumer/KafkaAvroConsumer.java
@@ -3,21 +3,36 @@ package com.kafkaavroschema.consumer;
 import com.kafkaavroschema.dto.Employee;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.DltHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.retry.annotation.Backoff;
 import org.springframework.stereotype.Service;
+import org.springframework.messaging.handler.annotation.Header;
+
 
 @Service
 @Slf4j
 public class KafkaAvroConsumer {
 
-
+    @RetryableTopic(attempts = "5", exclude = {NullPointerException.class}, backoff = @Backoff(delay = 2000, multiplier = 2.0))
     @KafkaListener(topics = "kafka-avro")
-    public void read(ConsumerRecord<String, Employee> consumerRecord) {
+    public void read(ConsumerRecord<String, Employee> consumerRecord, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic, @Header(KafkaHeaders.OFFSET) long offset) {
 
         String key = consumerRecord.key();
         Employee employee = consumerRecord.value();
 
-        log.info("Received record: " + key + ", " + employee);
+        if (employee.getEmailId().toString().endsWith("@retry.me")) {
+            throw new IllegalStateException("Forcing retries (email ends with @retry.me)");
+        }
 
+        log.info("Received record: {}, {}", key, employee);
+
+    }
+
+    @DltHandler
+    public void listenDLT(ConsumerRecord<String, Employee> consumerRecord, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic, @Header(KafkaHeaders.OFFSET) long offset) {
+        log.info("DLT Received : {} , from {} , offset {}", consumerRecord.value().getEmailId(),topic,offset);
     }
 }
